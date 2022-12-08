7ac79ee7730770e50b9712d9e5d84b2f14c77cef
Kushal Das <mail@kushaldas.in>
2022-12-08 10:59:50 +0100

    MOre dependency

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,5 +2,6 @@
 set +x
 python3 -m venv venv
 source ./venv/bin/activate
-python3 -m pip install -r requirements.txt
+python3 -m pip install wheel
+python3 -m pip install --require-hashes -r requirements.txt
 
